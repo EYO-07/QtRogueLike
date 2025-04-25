ceb19bb8a1249629487c99309b0412614d8b68ed
oZumbiAnalitico <52110875+EYO-07@users.noreply.github.com>
2025-04-25 14:27:35 -0300

    Add files via upload

---
+++ mods/mod_example.py
@@ -0,0 +1,9 @@
+# MOD EXAMPLE
+# 'Entry' will be executed before the game starts 
+
+# -- do things
+
+# -- Entry Point 
+def Entry(game_instance):
+    print( "HELLO WORLD MOD EXAMPLE" )
+    # game_instance.add_message( "HELLO WORLD MOD EXAMPLE" )
